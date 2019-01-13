967bbd6f6093d88878a81ff81ec26fe218c11844
ruegen <ruegen@icloud.com>
2019-01-13 12:03:53 +1100

    first commit, setup

---
+++ Gemfile
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+gem "money"
+
+group :development, :test do
+    gem 'guard' # NOTE: this is necessary in newer versions
+    gem 'guard-minitest'
+end
---
+++ spec/shop_spec.rb
@@ -0,0 +1,29 @@
+require "shop"
+
+orders = [
+  {qty: 10, code: :R12},
+  {qty: 15, code: :L09},
+  {qty: 13, code: :T58},
+]
+
+describe "shop" do
+  let(:orders) { orders }
+  result = "" "
+  10 R12 $12.99
+        1 x 10 $12.99 
+  15 L09 $41.90
+        1 x 9 $24.95
+        1 x 6 $16.95 
+  13 T58 $25.85
+        2 x 5 $9.95
+        1 x 3 $5.95
+  " ""
+
+  puts result
+
+  it "should print invoice" do
+    shop = Shop.new
+    shop.place_new_orders orders
+    expect(shop.print_last_invoice).must_equal result
+  end
+end
