299e3ec772eb7a2a314a350993540fe104df7a98
ruegen <ruegen@icloud.com>
2019-01-13 13:03:50 +1100

    change name to title

--- lib/bundle.rb
+++ lib/bundle.rb
@@ -1,13 +1,13 @@
 class Bundle
   @@bundles = nil
 
-  attr_accessor :code, :qty, :price_in_cents, :name
+  attr_accessor :code, :qty, :price_in_cents, :title
 
-  def initialize(code, qty, price_in_cents, name)
+  def initialize(code, qty, price_in_cents, title)
     @code = code
     @qty = qty
     @price_in_cents = price_in_cents
-    @name = name
+    @title = title
   end
 
   def self.bundles
--- spec/bundle_spec.rb
+++ spec/bundle_spec.rb
@@ -2,14 +2,13 @@ require "bundle"
 
 describe "bundle" do
   let(:code) { :R12 }
+  let(:title) { "Roses" }
+  let(:qty) { 10 }
+  let(:price_in_cents) { 1299 }
 
   it "should match initialize values" do
-    name = "Roses"
-    qty = 10
-    price_in_cents = 1299
-
-    bundle = Bundle.new code, qty, price_in_cents, name
-    expect(bundle.name).must_equal name
+    bundle = Bundle.new code, qty, price_in_cents, title
+    expect(bundle.title).must_equal title
     expect(bundle.code).must_equal code
     expect(bundle.qty).must_equal qty
     expect(bundle.price_in_cents).must_equal price_in_cents
@@ -17,6 +16,8 @@ describe "bundle" do
 
   it "should add bundle each initialize" do
     expect(Bundle.bundles).must_be_nil
+    bundle = Bundle.new code, qty, price_in_cents, title
+    expect(Bundle.bundles).must_include bundle
   end
 
   #   it "should return bundles by code" do
