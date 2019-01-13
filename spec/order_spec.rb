76b902e2f3e23d6eb377eb92f0c12cc2d3ee213e
ruegen <ruegen@icloud.com>
2019-01-14 10:28:59 +1100

    fix name of method

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,11 +1,9 @@
 require "order"
 
 describe "order" do
-  let(:order) { Order.get_order }
+  let(:order) { Order.load_order }
 
   it "should be an instance" do
     expect(order).must_be_instance_of Order
   end
-
-  it "should have a"
 end
