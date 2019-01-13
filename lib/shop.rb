8c2bca00d62dad405d7139cba0812d7e9b469496
ruegen <ruegen@icloud.com>
2019-01-14 10:29:18 +1100

    update methods to suit changes

--- lib/shop.rb
+++ lib/shop.rb
@@ -1,12 +1,17 @@
+require "order"
+
 class Shop
   def initialize
     @order = nil
   end
 
-  def issue_order(qty, code)
-    @order = Order.new qty code
+  def issue_order
+    @order = Order.load_order
   end
 
   def print_invoice
+    @order.line_items.each do |line_item|
+      line_item.
+    end
   end
 end
