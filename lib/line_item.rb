08f16c2aef9c3e92d9881ef49fad71ca1b77e8f2
ruegen <ruegen@icloud.com>
2019-01-14 10:27:50 +1100

    add print total method for line item

--- lib/line_item.rb
+++ lib/line_item.rb
@@ -1,18 +1,29 @@
 require "bundle"
+require "money"
 
 class LineItem
+  Money.locale_backend = :currency
   attr_accessor :code, :qty, :bundle, :best_fit
 
   def initialize(line_item)
     @code = line_item[:code]
     @qty = line_item[:qty]
     @bundle = Bundle.get_bundle_by_code @code
-    @best_fit = find_line_item_best_fit @qty, @bundle
+    best_fit = get_best_fit @qty, @bundle
+    @bundle_best_fit = bundle_by_best_fit best_fit, @bundle
+  end
+
+  def print_total
+    total_in_cents = @bundle_best_fit.reduce(0) do |total, bundle|
+      total + bundle.price_in_cents
+    end
+    total_in_dollars = Money.new(total_in_cents, "USD").format
+    "#{@qty} #{@code} #{total_in_dollars}"
   end
 
   private
 
-  def find_line_item_best_fit(qty, bundle)
+  def get_best_fit(qty, bundle)
     return nil if qty < 0
     return nil if qty != qty.floor
 
@@ -39,5 +50,17 @@ class LineItem
       qty = group
     end
     result.sort!.reverse!
+  end
+
+  def bundle_by_best_fit(best_fit, bundles)
+    bundles_by_best_fit = []
+    best_fit.each { |qty|
+      bundles.each { |bundle|
+        if bundle.qty == qty
+          bundles_by_best_fit.push bundle
+        end
+      }
+    }
+    bundles_by_best_fit
   end
 end
--- spec/line_item_spec.rb
+++ spec/line_item_spec.rb
@@ -11,20 +11,26 @@ describe "line item" do
   end
 
   it "should find best fit for bundle" do
-    expect(line_item.best_fit).must_equal [9, 6]
+    result = line_item.send(:get_best_fit, line_item.qty, line_item.bundle)
+    expect(result).must_equal [9, 6]
+  end
+
+  it "should print total of line item" do
+    expected = "15 L09 $41.90"
+    expect(line_item.print_total).must_equal expected
   end
 
   it "should find best fit for given quantity and bundle" do
     expect(
-      line_item.send(:find_line_item_best_fit, 10, Bundle.get_bundle_by_code(:R12))
+      line_item.send(:get_best_fit, 10, Bundle.get_bundle_by_code(:R12))
     ).must_equal [10]
 
     expect(
-      line_item.send(:find_line_item_best_fit, 15, Bundle.get_bundle_by_code(:L09))
+      line_item.send(:get_best_fit, 15, Bundle.get_bundle_by_code(:L09))
     ).must_equal [9, 6]
 
     expect(
-      line_item.send(:find_line_item_best_fit, 13, Bundle.get_bundle_by_code(:T58))
+      line_item.send(:get_best_fit, 13, Bundle.get_bundle_by_code(:T58))
     ).must_equal [5, 5, 3]
   end
 end
