7c3c8ca297231b27bd033188577c17d29bc53813
ruegen <ruegen@icloud.com>
2019-01-14 11:12:46 +1100

    remove hash as method argument

--- lib/order.rb
+++ lib/order.rb
@@ -13,12 +13,19 @@ class Order
       line_item[:code] = line_item[:code].to_sym
       LineItem.new line_item
     end
-    new(line_items: line_items)
+    new(line_items)
   end
 
   attr_accessor :line_items
 
   def initialize(line_items)
     @line_items = line_items
+  end
+
+  def print_line_items
+    @line_items.each do |line_item|
+      line_item.print_total
+      line_item.print_subtotal
+    end
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,9 +1,14 @@
 require "order"
+require "line_item"
 
 describe "order" do
   let(:order) { Order.load_order }
 
   it "should be an instance" do
     expect(order).must_be_instance_of Order
+  end
+
+  it "should return a line item" do
+    expect(order.line_items.first).must_be_instance_of LineItem
   end
 end
