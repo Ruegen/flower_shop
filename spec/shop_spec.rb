90fcb3ed242a3e42a46d55c6f1512324428d3135
ruegen <ruegen@icloud.com>
2019-01-14 11:11:50 +1100

    update spec for print_invoice method

--- spec/shop_spec.rb
+++ spec/shop_spec.rb
@@ -1,27 +1,21 @@
 require "shop"
 
-order = [
-  {qty: 10, code: :R12},
-  {qty: 15, code: :L09},
-  {qty: 13, code: :T58},
-]
-
 describe "shop" do
-  let(:orders) { orders }
-  result = "" "
-  10 R12 $12.99
-        1 x 10 $12.99 
-  15 L09 $41.90
-        1 x 9 $24.95
-        1 x 6 $16.95 
-  13 T58 $25.85
-        2 x 5 $9.95
-        1 x 3 $5.95
-  " ""
-
   it "should print invoice" do
-    # shop = Shop.new
-    # shop.issue_order order
-    # expect(shop.print_invoice).must_equal result
+    result = "" "10 R12 $12.99
+\t1 x 10 $12.99
+15 L09 $41.90
+\t1 x 9 $24.95
+\t1 x 6 $16.95
+13 T58 $25.85
+\t2 x 5 $9.95
+\t1 x 3 $5.95
+" ""
+    shop = Shop.new
+    shop.issue_order
+    out, err = capture_io do
+      shop.print_invoice
+    end
+    expect(out).must_equal result
   end
 end
